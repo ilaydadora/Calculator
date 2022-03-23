82ae720f484264e3d2154a2436cfc834a1bebc5e
ilaydadora <100840400+ilaydadora@users.noreply.github.com>
2022-03-23 14:56:05 +0300

    Simple Calculator
    Basic calculator

---
+++ SimpleCalculator/SimpleCalculator/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by İlayda Metin on 25.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstText: UITextField!
+    
+    
+    @IBOutlet weak var secondText: UITextField!
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+    
+    
+    
